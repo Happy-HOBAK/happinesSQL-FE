6ff20ac9f556f405d0a08f8aed6eafb000cd1d96
solmee <kii5541@naver.com>
2024-03-29 17:17:35 +0900

    네비게이션 바 이미지 추가

--- App.js
+++ App.js
@@ -1,61 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from './src/screens/home/home.js';
-import Test from './src/screens/home/test.js';
-//import { HomeScreen, RecordsScreen, ReportScreen, CalenderScreen, TrendScreen } from './src/common/bottomnavigation';
+import { Entypo } from '@expo/vector-icons';
+import { ReportScreen, CalenderScreen, RecordsScreen, HomeScreen, TrendScreen } from './src/common/navigation.js';
+import homeicon from './src/common/icons/home.png';
+import recordicon from './src/common/icons/record.png';
+import reporticon from './src/common/icons/report.png';
+import trendicon from './src/common/icons/trend.png';
+import calendericon from './src/common/icons/calender.png';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
- function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
- function CalenderScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Calender!</Text>
-    </View>
-  );
-}
- function RecordsScreen() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Records!</Text>
-      </View>
-    );
-  }
- function ReportScreen() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Report!</Text>
-      </View>
-    );
-  }
-
- function TrendScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Trend!</Text>
-    </View>
-  );
-}
-
 function MyTabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="레포트 확인" component={ReportScreen} />
-      <Tab.Screen name="행복 달력" component={CalenderScreen} />
-      <Tab.Screen name="홈" component={HomeScreen} />
-      <Tab.Screen name="행복 트렌드" component={TrendScreen} />
-      <Tab.Screen name="행복 톺아보기" component={RecordsScreen} />
+      <Tab.Screen name="레포트 확인" component={ReportScreen} 
+      options={{tabBarIcon: ({ color, size }) => (
+            <Image source={reporticon} style={{ width: 24, height: 24 }}/>
+          ),}}/>
+      <Tab.Screen name="행복 달력" component={CalenderScreen}
+      options={{tabBarIcon: ({ color, size }) => (
+        <Image source={calendericon} style={{ width: 24, height: 24 }}/>
+        ),}} />
+      <Tab.Screen name="홈" component={HomeScreen} 
+      options={{tabBarIcon: ({ color, size }) => (
+        <Image source={homeicon} style={{ width: 24, height: 24 }}/>
+        ),}}/>
+      <Tab.Screen name="행복 트렌드" component={TrendScreen} 
+      options={{tabBarIcon: ({ color, size }) => (
+        <Image source={trendicon} style={{ width: 24, height: 24 }}/>
+        ),}}/>
+      <Tab.Screen name="행복 톺아보기" component={RecordsScreen} 
+      options={{tabBarIcon: ({ color, size }) => (
+        <Image source={recordicon} style={{ width: 24, height: 24 }}/>
+        ),}}/>
     </Tab.Navigator>
   );
 }
