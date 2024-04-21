2bf5da8bf0628b020e9e2e100b5234ea737ffeb8
solmee <kii5541@naver.com>
2024-04-21 21:05:48 +0900

    홈(2) 활동 추가 기능 수정

--- src/screens/home/assets/components/dropdown.js
+++ src/screens/home/assets/components/dropdown.js
@@ -1,28 +1,34 @@
-import React, { useState } from 'react';
-import { StyleSheet } from 'react-native';
-import { Dropdown } from 'react-native-element-dropdown';
-import { theme } from '../../../../styles/theme';
-import { categorykData } from '../../../../common/data/category';
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+import { Dropdown } from "react-native-element-dropdown";
+import { theme } from "../../../../styles/theme";
+import { categorykData } from "../../../../common/data/category";
 
-const DropdownComponent = () => {
-  const categoryNames = categorykData.categories.map(category => category.name);
+const DropdownComponent = ({ onSelectCategory }) => {
+  const categoryNames = categorykData.categories.map(
+    (category) => category.name
+  );
+
+  const handleSelectCategory = (item) => {
+    setValue(item.value);
+    onSelectCategory(item.value);
+  };
 
   const [value, setValue] = useState(null);
+  //console.log(value);
 
   return (
     <Dropdown
       style={styles.dropdown}
-      data={categoryNames.map((name, index) => ({ label: name, value: index.toString() }))}
+      data={categoryNames.map((name, index) => ({ label: name, value: index }))}
       search
       maxHeight={300}
       labelField="label"
       valueField="value"
-      placeholder="취미 및 여가활동"
+      placeholder="취미 및 여가 활동"
       searchPlaceholder="검색하기"
       value={value}
-      onChange={item => {
-        setValue(item.value);
-      }}
+      onChange={handleSelectCategory}
     />
   );
 };
@@ -33,7 +39,7 @@ const styles = StyleSheet.create({
   dropdown: {
     margin: 16,
     height: 45,
-    width:200,
+    width: 200,
     borderBottomColor: theme.grey_2,
   },
 });
--- src/screens/home/assets/components/modal.js
+++ src/screens/home/assets/components/modal.js
@@ -7,8 +7,21 @@ import DropdownComponent from "./dropdown";
 const ModalScreen = ({ onClose }) => {
     //const [isModalVisible, setIsModalVisible] = useState(true);
 
+    const [selectedValue, setSelectedValue] = useState(1);
+    const [activityName, setActivityName] = useState('');
+
+    const handleSelectedCategory = (value) => {
+        setSelectedValue(value + 1);
+    }
+
+    const handleActivityNameChange = (text) => {
+        setActivityName(text);
+    }
+
     const onPressModalClose = () => {
         console.log("팝업을 닫는 중 입니다");
+        console.log(selectedValue);
+        console.log(activityName);
         onClose();
     }
 
@@ -22,12 +35,15 @@ const ModalScreen = ({ onClose }) => {
                </View>
                 <View style={{alignSelf:'flex-start'}}>
                 <Text style={styles.modalTextStyle}>카테고리 설정</Text>
-                <DropdownComponent/>
+                <DropdownComponent onSelectCategory = {handleSelectedCategory}/>
                 </View>
                 <View style={{alignSelf:'flex-start'}}>
                 <Text style={styles.modalTextStyle}>활동 입력하기</Text>
                 </View>
-                <PlusInput/>
+                <PlusInput 
+                onChangeText={handleActivityNameChange}
+                value={activityName}
+                placeholder="활동명 입력"/>
                 <PlusBtn onPress={onPressModalClose}>
                     <PlusText>활동 추가하기</PlusText>
                 </PlusBtn>
