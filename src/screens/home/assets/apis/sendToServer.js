23fa549205a615ca46c00875812430f1aca3defe
solmee <kii5541@naver.com>
2024-04-21 21:14:05 +0900

    fetch to axios 변경

--- src/screens/home/assets/apis/sendToServer.js
+++ src/screens/home/assets/apis/sendToServer.js
@@ -1,11 +1,41 @@
+// export const sendToServer = async (emotion, activityId, memo, location, image, latitude, longitude, country) => {
+//     try {
+//       const response = await fetch('http://example.com/saveData', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//             happiness: emotion,
+//             activityId: activityId,
+//             full_name: location,
+//             country: country,
+//             city: location.split(' ')[0],
+//             district: location.split(' ')[1],
+//             x_pos: latitude,
+//             y_pos: longitude,
+//             memo: memo,
+//             img: image,
+//         }),
+//       });
+  
+//       if (!response.ok) {
+//         throw new Error('서버 응답이 실패했습니다.');
+//       }
+  
+//       const data = await response.json();
+//       return data; 
+//     } catch (error) {
+//       console.error('서버로 데이터 전송 중 오류 발생:', error);
+//       throw error;
+//     }
+//   };
+
+import axios from 'axios';
+
 export const sendToServer = async (emotion, activityId, memo, location, image, latitude, longitude, country) => {
     try {
-      const response = await fetch('http://example.com/saveData', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
+        const response = await axios.post('http://example.com/api/records', {
             happiness: emotion,
             activityId: activityId,
             full_name: location,
@@ -16,18 +46,21 @@ export const sendToServer = async (emotion, activityId, memo, location, image, l
             y_pos: longitude,
             memo: memo,
             img: image,
-        }),
-      });
-  
-      if (!response.ok) {
-        throw new Error('서버 응답이 실패했습니다.');
-      }
-  
-      const data = await response.json();
-      return data; 
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (!response.data) {
+            throw new Error('서버 응답이 실패했습니다.');
+        }
+
+        return response.data;
     } catch (error) {
-      console.error('서버로 데이터 전송 중 오류 발생:', error);
-      throw error;
+        console.error('서버로 데이터 전송 중 오류 발생:', error);
+        throw error;
     }
-  };
+};
+
   
