27b72034c876c0d6c09b05b42faf5439a49f2b57
solmee <kii5541@naver.com>
2024-05-05 00:17:04 +0900

    푸시 알림 로컬 상에서 구현

--- App.js
+++ App.js
@@ -1,38 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { GlobalStyle } from './src/styles/Globalstyle.js';
-import Home from './src/screens/home/home.js';
-import Test from './src/screens/home/test.js';
-import UserInfo from './src/screens/home/userInfo.js';
-import MyTabs from './src/common/navigation/tabs.js';
-import DataScreen from './src/screens/report/assets/components/DataScreen.js';
-import { RecoilRoot } from 'recoil';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { scheduleNotification } from "./src/common/notification/notificationUtils.js";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { GlobalStyle } from "./src/styles/Globalstyle.js";
+import Home from "./src/screens/home/home.js";
+import Test from "./src/screens/home/test.js";
+import UserInfo from "./src/screens/home/userInfo.js";
+import MyTabs from "./src/common/navigation/tabs.js";
+import DataScreen from "./src/screens/report/assets/components/DataScreen.js";
+import { RecoilRoot } from "recoil";
+import * as Notifications from "expo-notifications";
+import React, { useEffect } from "react";
+import { scheduleNotifications } from "./src/common/notification/notificationUtils.js";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+    }),
+  });
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Notifications.requestPermissionsAsync();
+      if (status !== "granted") {
+        alert("알람 설정이 거부되었습니다.");
+      } else {
+        await scheduleNotifications();
+      }
+    })();
+  }, []);
+
   return (
     <RecoilRoot>
-    <NavigationContainer theme={{
-        ...DefaultTheme,
-        colors: {
-          ...DefaultTheme.colors,
-          background: 'white',
-          text: '#878787'
-        },
-      }}
-      independent={true}>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Tabs" component={MyTabs} />
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Test" component={Test} />
-        <Stack.Screen name="UserInfo" component={UserInfo} />
-        <Stack.Screen name="Data" component={DataScreen} />
-      </Stack.Navigator>
-      <StatusBar style="dark" />
-    </NavigationContainer>
+      <NavigationContainer
+        theme={{
+          ...DefaultTheme,
+          colors: {
+            ...DefaultTheme.colors,
+            background: "white",
+            text: "#878787",
+          },
+        }}
+        independent={true}
+      >
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Tabs" component={MyTabs} />
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Test" component={Test} />
+          <Stack.Screen name="UserInfo" component={UserInfo} />
+          <Stack.Screen name="Data" component={DataScreen} />
+        </Stack.Navigator>
+        <StatusBar style="dark" />
+      </NavigationContainer>
     </RecoilRoot>
   );
 }
---
+++ src/common/notification/notificationUtils.js
@@ -0,0 +1,64 @@
+import * as Notifications from "expo-notifications";
+//import { sendPushNotification } from "expo-notifications";
+
+// export async function sendNotification(expoPushToken) {
+//   await sendPushNotification({
+//     to: expoPushToken,
+//     title: "Hello World!",
+//     body: "This is a test notification.",
+//     sound: "default",
+//   });
+// }
+
+export async function scheduleNotifications() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "Welcome!",
+      body: "테스트입니댜",
+      sound: "default",
+    },
+    trigger: null, // 테스트
+  });
+
+  const hours = [0, 9, 12, 15, 18, 23];
+  for (let hour of hours) {
+    const trigger = {
+      hour: hour,
+      minute: 14,
+      repeats: true,
+    };
+
+    const notificationContent = {
+      title: "시간 테스트",
+      body: "ㅠㅠ",
+      sound: "default",
+    };
+
+    await Notifications.scheduleNotificationAsync({
+      content: notificationContent,
+      trigger: trigger,
+    });
+  }
+}
+
+export async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  // 서버에 토큰 저장 로직
+}
